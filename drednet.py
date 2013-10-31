35326f332601093b17f4c94286aec512bef8254a
kurotaki.hiroki <kurotaki.hiroki@unipro.co.jp>
2013-11-01 00:07:29 +0900

    drednet init

---
+++ drednet.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import cPickle
+import gzip
+import sys
+from pprint import pprint
+
+import numpy
+import theano
+
+from logistic_sgd import LogisticRegression, load_data
+
+
+if __name__ == '__main__':
+    print('a')
