33dfd04bad35504a35a5a1f4941656f276fd7e72
kurotaki.hiroki <kurotaki.hiroki@unipro.co.jp>
2013-11-01 04:57:25 +0900

    とりあえずConvNetLayerとつなげた

--- drednet.py
+++ drednet.py
@@ -7,42 +7,53 @@ from pprint import pprint
 
 import numpy
 import theano
+import theano.tensor as T
 
 sys.path.append('tutorial_code')
 
 from logistic_sgd import load_data
 from showPklGz import showDataset, theanoTensor2NumpyArray
+from convolutional_mlp import LeNetConvPoolLayer
 
-def minimizeDataset(input='data/mnist.pkl.gz', output='data/minimized.pkl.gz'):
+class DredNetLayer(object):
+    def __init__(self, rng, input):
+        self.input = input
+        self.output = input
+        self.params = []
+
+def test_drednet(learning_rate=0.1, n_epochs=200, input='data/mnist_100.pkl.gz', nkerns=[20, 50], batch_size=50):
     print('read ' + input)
+    rng = numpy.random.RandomState(23455)
+
     datasets = load_data(input)
-    
     train_set_x, train_set_y = datasets[0]
     valid_set_x, valid_set_y = datasets[1]
     test_set_x, test_set_y = datasets[2]
 
-    train_len = 500
-    valid_len = 100
-    test_len = 100
+    # compute number of minibatches for training, validation and testing
+    n_train_batches = train_set_x.get_value(borrow=True).shape[0]
+    n_valid_batches = valid_set_x.get_value(borrow=True).shape[0]
+    n_test_batches = test_set_x.get_value(borrow=True).shape[0]
+    n_train_batches /= batch_size
+    n_valid_batches /= batch_size
+    n_test_batches /= batch_size
 
-    minimized_train_x = theanoTensor2NumpyArray(train_set_x[0:train_len-1:1])
-    minimized_valid_x = theanoTensor2NumpyArray(valid_set_x[0:valid_len-1:1])
-    minimized_test_x = theanoTensor2NumpyArray(test_set_x[0:test_len-1:1])
+    # allocate symbolic variables for the data
+    index = T.lscalar()  # index to a [mini]batch
+    x = T.matrix('x')   # the data is presented as rasterized images
+    y = T.ivector('y')  # the labels are presented as 1D vector of
+                        # [int] labels
     
-    minimized_train_y = theanoTensor2NumpyArray(train_set_y[0:train_len-1:1])
-    minimized_valid_y = theanoTensor2NumpyArray(train_set_y[0:valid_len-1:1])
-    minimized_test_y = theanoTensor2NumpyArray(train_set_y[0:test_len-1:1])
+    ishape = (28, 28)  # this is the size of MNIST images
 
-    data = (minimized_train_x, minimized_train_y),(minimized_valid_x, minimized_valid_y), (minimized_test_x, minimized_test_y)
+    layer0_input = x.reshape((batch_size, 1, 28, 28))
+    layer0 = LeNetConvPoolLayer(rng, input=layer0_input,
+            image_shape=(batch_size, 1, 28, 28),
+                                filter_shape=(nkerns[0], 1, 5, 5), poolsize=(2, 2))
+    layer1 = DredNetLayer(rng, input=layer0.output)
+    pprint(layer1.output)
 
-    f = gzip.open(output, 'wb')
-    cPickle.dump(data, f, -1)
-    f.close()
-
-    print('output to ' + output)
 
 
 if __name__ == '__main__':
-    #showDataset(dataset)
-    minimizeDataset('data/mnist.pkl.gz', 'data/mnist_100.pkl.gz')
-    
+    test_drednet()
