6c0715021897f24c60ca859f426814f815e85757
kurotaki.hiroki <kurotaki.hiroki@unipro.co.jp>
2013-11-08 13:10:15 +0900

    dataをconvert/outputしてload_file成功

--- convert_cifar10.py
+++ convert_cifar10.py
@@ -63,34 +63,11 @@ def convert_cifar10(output='data/cifar10_dlt_compatible.pkl.gz'):
                 numpy.asarray(test_dir['labels']))
     to_pickle = (train_set, valid_set, test_set)
 
+    return
+    f = gzip.open(output, 'wb')
+    cPickle.dump(to_pickle, f, protocol=cPickle.HIGHEST_PROTOCOL)
+    f.close()
 
-
-
-
-    #datasets = load_data(input)
-    #
-    #train_set_x, train_set_y = datasets[0]
-    #valid_set_x, valid_set_y = datasets[1]
-    #test_set_x, test_set_y = datasets[2]
-    #
-    #train_len = 500
-    #valid_len = 100
-    #test_len = 100
-    #
-    #minified_train_x = theanoTensor2NumpyArray(train_set_x[0:train_len:1])
-    #minified_valid_x = theanoTensor2NumpyArray(valid_set_x[0:valid_len:1])
-    #minified_test_x = theanoTensor2NumpyArray(test_set_x[0:test_len:1])
-    #
-    #minified_train_y = theanoTensor2NumpyArray(train_set_y[0:train_len:1])
-    #minified_valid_y = theanoTensor2NumpyArray(train_set_y[0:valid_len:1])
-    #minified_test_y = theanoTensor2NumpyArray(train_set_y[0:test_len:1])
-    #
-    #data = (minified_train_x, minified_train_y),(minified_valid_x, minified_valid_y), (minified_test_x, minified_test_y)
-    #
-    #f = gzip.open(output, 'wb')
-    #cPickle.dump(data, f, -1)
-    #f.close()
-    #
     print('output to ' + output)
 
 
