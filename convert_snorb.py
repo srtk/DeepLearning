34a3b1f78028aeb6f3eee431573a7fade6dec71d
kurotaki.hiroki <kurotaki.hiroki@unipro.co.jp>
2013-11-12 05:38:48 +0900

    dat読み取りをuint8に修正、読み取り成功

--- convert_snorb.py
+++ convert_snorb.py
@@ -12,8 +12,88 @@ import operator
 import numpy as np
 import theano
 
-#http://sunabako.sblo.jp/article/51698189.html
-#convert to the format loadable with logistic_sgd.load_data
+def check_file(file):
+    print('reading data from  ' + file)
+    if not os.path.exists(file):
+        print(file + ' not exists')
+        exit(1)
+
+
+def open_file(file):
+    check_file(file)
+    return gzip.open(file, 'rb')
+
+def read_header(file):
+    magic, ndim = struct.unpack('<ii', file.read(4 * 2))
+
+    n_dim_info = max([ndim, 3]) #see the quotation above
+    dims = []
+    for i in range(0, n_dim_info):
+        dims.extend(struct.unpack('<i', file.read(4)))
+    dims = dims[:ndim]
+    return magic, ndim, dims
+
+
+def read_from_datafile(file):
+    """
+    Each "-dat" file stores 24,300 image pairs
+    (5 categories, 5 instances, 6 lightings, 9 elevations, and 18 azimuths).
+    The corresponding "-cat" file contains 24,300 category labels
+    (0 for animal, 1 for human, 2 for plane, 3 for truck, 4 for car).
+    ...
+    Note that when the matrix has less than 3 dimensions,
+    say, it's a 1D vector, then dim[1] and dim[2] are both 1.
+    When the matrix has more than 3 dimensions,
+    the header will be followed by further dimension size information.
+    """
+
+    #header
+
+    f = open_file(file)
+    magic, ndim, dims = read_header(f)
+
+    #data
+
+    n_values_in_data = reduce(operator.mul, dims[1:]) if (len(dims) > 2) else 1
+    value_size = 1
+    unpack_datatype = 'B'
+    unpack_format = '<%d%s' % (n_values_in_data, unpack_datatype)
+    datas = []
+    #for data_index in range(0, dim[0]):
+    for data_index in range(0, 3):
+        #the values stored as uint8
+        tmp = struct.unpack(unpack_format, f.read(value_size * n_values_in_data))
+        datas.append(list(tmp))
+
+    f.close()
+    return datas
+
+def read_from_categoryfile(file):
+    #header
+    f = open_file(file)
+    magic, ndim, dims = read_header(f)
+
+    #data
+    n_values_in_data = reduce(operator.mul, dims[1:]) if (len(dims) > 2) else 1
+    value_size = 4
+    unpack_datatype = 'i'
+    unpack_format = '<%d%s' % (n_values_in_data, unpack_datatype)
+    datas = []
+    for data_index in range(0, dims[0]):
+        #the values stored as uint32
+        tmp = struct.unpack(unpack_format, f.read(value_size * n_values_in_data))
+        datas.append(list(tmp))
+
+    f.close()
+    return datas
+
+"""
+ convert to the format loadable with logistic_sgd.load_data
+
+ although the files have extensions 'mat' they can't be loaded with scipy.io.loadmat
+ actually their format is not matlab's but an original one
+ see http://www.cs.nyu.edu/~ylclab/data/norb-v1.0-small/
+"""
 def convert_small_norb(output='data/smallnorb_for_dlt.pkl.gz'):
     if os.path.exists(output):
         print(output + ' already exists. remove it manually for safety')
@@ -27,50 +107,8 @@ def convert_small_norb(output='data/smallnorb_for_dlt.pkl.gz'):
     test_cat = os.path.join(input_dir, 'smallnorb-5x01235x9x18x6x2x96x96-testing-cat.mat.gz')
     test_info = os.path.join(input_dir, 'smallnorb-5x01235x9x18x6x2x96x96-testing-info.mat.gz')
 
-
-    def check_file(file):
-        print('reading data from  ' + file)
-        if not os.path.exists(file):
-            print(file + ' not exists')
-            exit(1)
-
-    def open_file(file):
-        check_file(file)
-        return gzip.open(file, 'rb')
-
-    """
-     Each "-dat" file stores 24,300 image pairs
-     (5 categories, 5 instances, 6 lightings, 9 elevations, and 18 azimuths).
-     The corresponding "-cat" file contains 24,300 category labels
-     (0 for animal, 1 for human, 2 for plane, 3 for truck, 4 for car).
-    """
-    """
-     Note that when the matrix has less than 3 dimensions,
-     say, it's a 1D vector, then dim[1] and dim[2] are both 1.
-     When the matrix has more than 3 dimensions,
-     the header will be followed by further dimension size information.
-    """
-    #at first read the header only
-    f = open_file(train_dat)
-    #magic, ndim, dim0, dim1, dim2, dim3 = struct.unpack('<iiiiii', f.read(4*6))
-    magic, ndim = struct.unpack('<ii', f.read(4*2))
-
-    n_dim_info = max([ndim, 3]) #see the quotation above
-    dims = []
-    for i in range(0, n_dim_info):
-        dims.extend(struct.unpack('<i', f.read(4)))
-    dims = dims[:ndim]
-
-    n_values_in_data = reduce(operator.mul, dims[1:]) if (len(dims) > 2) else 1
-    unpack_format = '<%dI' % n_values_in_data
-    datas = []
-    #for data_index in range(0, dims[0]):
-    for data_index in range(0, 9):
-        tmp =struct.unpack(unpack_format, f.read(4 * n_values_in_data))
-        datas.append(list(tmp))
-
-    f.close()
-
+    arr_train_dat = read_from_datafile(train_dat)
+    arr_train_cat = read_from_categoryfile(train_cat)
     print('bp placehoder')
 
     exit(0)
