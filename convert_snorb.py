e6daa036b8ef2b968de252946d3b9d9ac49061b7
kurotaki.hiroki <kurotaki.hiroki@unipro.co.jp>
2013-11-11 09:15:49 +0900

    gzip読めないのを見逃して無駄なものを作ったが一応記録

--- convert_snorb.py
+++ convert_snorb.py
@@ -7,32 +7,36 @@ import os.path
 import cPickle
 from pprint import pprint
 import struct
+import operator
 
-import numpy
+import numpy as np
 import theano
 
 #http://sunabako.sblo.jp/article/51698189.html
 #convert to the format loadable with logistic_sgd.load_data
 def convert_small_norb(output='data/smallnorb_for_dlt.pkl.gz'):
-    input_dir = 'data'
-    train_dat = 'smallnorb-5x46789x9x18x6x2x96x96-training-dat.mat.gz'
-    train_cat = 'smallnorb-5x46789x9x18x6x2x96x96-training-cat.mat.gz'
-    train_info = 'smallnorb-5x46789x9x18x6x2x96x96-training-info.mat.gz'
-    test_dat = 'smallnorb-5x01235x9x18x6x2x96x96-testing-dat.mat.gz'
-    test_cat = 'smallnorb-5x01235x9x18x6x2x96x96-testing-cat.mat.gz'
-    test_info = 'smallnorb-5x01235x9x18x6x2x96x96-testing-info.mat.gz'
-
     if os.path.exists(output):
         print(output + ' already exists. remove it manually for safety')
         return
 
-    def open_file(file):
-        input_path = os.path.join(input_dir, file)
-        print('reading data from  ' + input_path)
-        if not os.path.exists(input_path):
-            print(input_path + ' not exists')
+    input_dir = 'data'
+    train_dat = os.path.join(input_dir, 'smallnorb-5x46789x9x18x6x2x96x96-training-dat.mat.gz')
+    train_cat = os.path.join(input_dir, 'smallnorb-5x46789x9x18x6x2x96x96-training-cat.mat.gz')
+    train_info = os.path.join(input_dir, 'smallnorb-5x46789x9x18x6x2x96x96-training-info.mat.gz')
+    test_dat = os.path.join(input_dir, 'smallnorb-5x01235x9x18x6x2x96x96-testing-dat.mat.gz')
+    test_cat = os.path.join(input_dir, 'smallnorb-5x01235x9x18x6x2x96x96-testing-cat.mat.gz')
+    test_info = os.path.join(input_dir, 'smallnorb-5x01235x9x18x6x2x96x96-testing-info.mat.gz')
+
+
+    def check_file(file):
+        print('reading data from  ' + file)
+        if not os.path.exists(file):
+            print(file + ' not exists')
             exit(1)
-        return gzip.open(input_path, 'rb')
+
+    def open_file(file):
+        check_file(file)
+        return gzip.open(file, 'rb')
 
     """
      Each "-dat" file stores 24,300 image pairs
@@ -40,22 +44,35 @@ def convert_small_norb(output='data/smallnorb_for_dlt.pkl.gz'):
      The corresponding "-cat" file contains 24,300 category labels
      (0 for animal, 1 for human, 2 for plane, 3 for truck, 4 for car).
     """
+    """
+     Note that when the matrix has less than 3 dimensions,
+     say, it's a 1D vector, then dim[1] and dim[2] are both 1.
+     When the matrix has more than 3 dimensions,
+     the header will be followed by further dimension size information.
+    """
+    #at first read the header only
     f = open_file(train_dat)
-    try:
-        #header
-        # The magic number encodes the element type of the matrix:
-        #magic, ndim, dim0, dim1, dim2, dim3 = struct.unpack('<iiiiii', f.read(4*6))
-        magic, ndim = struct.unpack('<ii', f.read(4*2))
-        dims = []
-        for i in range(0, max([ndim, 3])):
-            dims.extend(struct.unpack('<i', f.read(4)))
+    #magic, ndim, dim0, dim1, dim2, dim3 = struct.unpack('<iiiiii', f.read(4*6))
+    magic, ndim = struct.unpack('<ii', f.read(4*2))
 
-        print('bp placehoder')
+    n_dim_info = max([ndim, 3]) #see the quotation above
+    dims = []
+    for i in range(0, n_dim_info):
+        dims.extend(struct.unpack('<i', f.read(4)))
 
-    except Exception as e:
-        pprint (e)
-    finally:
-        f.close()
+    f.close()
+
+    #construct dtype for numpy
+    header_format = [('magic', np.uint32)]
+    header_format.extend([('dim'+str(i), np.uint32) for i in range(0, ndim)])
+    n_data = reduce(operator.mul, dims)
+    dt = np.dtype([('header', header_format), ('data', np.uint32)])
+
+    #read the file again using numpy
+    array = np.fromfile(train_dat, dtype=dt)
+
+    print('bp placehoder')
+    f.close()
 
     exit(0)
 
